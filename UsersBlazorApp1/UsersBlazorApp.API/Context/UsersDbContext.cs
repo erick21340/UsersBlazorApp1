8fc6ef7d037a56c2df6631288f26a6ea452c247d
Erick20247 <Torot0086@gmail.vom>
2024-07-08 17:24:42 -0400

    eh creado le contexto

---
+++ UsersBlazorApp1/UsersBlazorApp.API/Context/UsersDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UsersBlazorApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace UsersBlazorApp.API.Context;
+
+public partial class UsersDbContext : DbContext
+{
+    public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) { }
+
+    public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
+
+    public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
+
+    public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
+
+    public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AspNetUsers>(entity =>
+        {
+            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "AspNetUserRoles",
+                    r => r.HasOne<AspNetRoles>().WithMany().HasForeignKey("RoleId"),
+                    l => l.HasOne<AspNetUsers>().WithMany().HasForeignKey("UserId"),
+                    j =>
+                    {
+                        j.HasKey("UserId", "RoleId");
+                    });
+        });
+
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ UsersBlazorApp1/UsersBlazorApp.API/Context/bdcontextfactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UsersBlazorApp.API.Context
+{
+    public class ContextFactory : IDbContextFactory<UsersDbContext>
+    {
+        private readonly DbContextOptions<UsersDbContext> _options;
+
+        public ContextFactory(DbContextOptions<UsersDbContext> options)
+        {
+            _options = options;
+        }
+
+        public UsersDbContext CreateDbContext()
+        {
+            return new UsersDbContext(_options);
+        }
+    }
+}
