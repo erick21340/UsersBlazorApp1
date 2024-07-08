bcb9508281aa43e5330e16a7f1b2ac16ed3dd4eb
Erick20247 <Torot0086@gmail.vom>
2024-07-08 17:23:37 -0400

    eh creado la interfaz

---
+++ UsersBlazorApp1/UsersBlazorApp.DATA/Interfaz/IApiService.cs
@@ -0,0 +1,10 @@
+﻿namespace UsersBlazorApp.Data.Interfaces;
+
+public interface IApiService<T>
+{
+    Task<List<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T user);
+    Task<bool> UpdateAsync(T user);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ UsersBlazorApp1/UsersBlazorApp.DATA/Interfaz/IClientService.cs
@@ -0,0 +1,10 @@
+﻿namespace UsersBlazorApp.Data.Interfaces;
+
+public interface IClientService<T>
+{
+    Task<List<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(int id);
+}
