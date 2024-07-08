d5591373ee657ce88ab3955fd5815fc1e79302e2
Erick20247 <Torot0086@gmail.vom>
2024-07-08 17:23:20 -0400

    eh creado el modelo

---
+++ UsersBlazorApp1/UsersBlazorApp.DATA/Models/AspNetRoles.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsersBlazorApp.Data.Models;
+
+public partial class AspNetRoles
+{
+	[Key]
+	public int Id { get; set; }
+
+	[StringLength(256)]
+	public string? Name { get; set; }
+
+	[InverseProperty("Role")]
+	public virtual ICollection<AspNetRoleClaims> AspNetRoleClaims { get; set; } = new List<AspNetRoleClaims>();
+
+	[InverseProperty("Roles")]
+	public virtual ICollection<AspNetUsers> Users { get; set; } = new List<AspNetUsers>();
+}
---
+++ UsersBlazorApp1/UsersBlazorApp.DATA/Models/AspNetUsers.cs
@@ -0,0 +1,31 @@
+﻿// AspNetUsers.cs
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsersBlazorApp.Data.Models;
+
+public partial class AspNetUsers
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(256)]
+    public string? UserName { get; set; }
+
+    [StringLength(256)]
+    public string? Email { get; set; }
+
+    public string? PasswordHash { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public DateTimeOffset? LockoutEnd { get; set; }
+
+    [InverseProperty("User")]
+    public virtual ICollection<AspNetUserClaims> AspNetUserClaims { get; set; } = new List<AspNetUserClaims>();
+
+    [InverseProperty("Users")]
+    public virtual ICollection<AspNetRoles> Roles { get; set; } = new List<AspNetRoles>();
+}
